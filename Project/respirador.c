4ea1c5c86c98af135964e5232a809eb5253d7565
Pablo Ferrero <ferreronicolas@hotmail.com>
2020-07-03 19:09:52 -0300

    Make clean + completo respirador

--- Project/respirador.c
+++ Project/respirador.c
@@ -64,7 +64,7 @@ void vEnviarInfo (void *p) {
 	BaseType_t xStatus;
 	TickType_t xLastWakeTime = xTaskGetTickCount();
 	for(;;) {
-		sprintf(buff, "niv_pres %d | prox_t %d | curr_t %d \n", nivel_limite_presion, pdMS_TO_TICKS(tiempo_respiracion), xLastWakeTime);
+		sprintf(buff, "niv_pres %d | prox_t %4d | curr_t %d", nivel_limite_presion, pdMS_TO_TICKS(tiempo_respiracion), xLastWakeTime);
 		xStatus = xQueueSend(infoQueue, buff, (TickType_t) 0);
 		if( xStatus != pdPASS) {
 			printf("Could not send to the queue.\n" );
@@ -74,20 +74,20 @@ void vEnviarInfo (void *p) {
 	}
 }
 
+// Si una respiración excede los 1.2s se debe generar una alarma
 void vNotificarAlarma (void *p) {
-	TickType_t xLastWakeTime = xTaskGetTickCount();
+	const TickType_t xDelay1200ms = pdMS_TO_TICKS( 1200 );
 	for(;;) {
-		xTaskNotify(xImprimirAlarmaHandle, 0x1, eSetValueWithOverwrite);
-		// La task se bloquea por exactamente 500ms
-		vTaskDelayUntil( &xLastWakeTime, xDelay500ms );
+		if( xSemaphoreTake(xSemaforoRespiracion, xDelay1200ms) == pdFALSE ) {
+			xTaskNotify(xImprimirAlarmaHandle, 0x2, eSetValueWithOverwrite);
+		}
 	}
+
 }
 
+// Después de cada respiración realizada, según el nivel de presión se debe ajustar el tiempo de respiración
 void vManejarRespiracion (void *p){
 	for(;;) {
-		if( xSemaphoreTake(xSemaforoRespiracion, 0) == pdPASS ) {
-			printf("Semaforo\n");
-		}
 		switch(nivel_limite_presion) {
 		case 0:
 			tiempo_respiracion = 1000;
@@ -96,6 +96,7 @@ void vManejarRespiracion (void *p){
 			tiempo_respiracion = 700;
 			break;
 		case 2:
+			// Si el nivel de presión es 2, además de ajustar el tiempo de respiración, se debe generar una alarma
 			tiempo_respiracion = 500;
 			xTaskNotify(xImprimirAlarmaHandle, 0x4, eSetValueWithOverwrite);
 			break;
@@ -106,28 +107,27 @@ void vManejarRespiracion (void *p){
 	}
 }
 
-int main(void){
+// Se deben enviar alarmas a display cada 0.5s
+void vAlarmaRespiracion (void *p) {
+	TickType_t xLastWakeTime = xTaskGetTickCount();
+	for(;;) {
+		xTaskNotify(xImprimirAlarmaHandle, 0x1, eSetValueWithoutOverwrite);
+		// La task se bloquea por exactamente 500ms
+		vTaskDelayUntil( &xLastWakeTime, xDelay500ms );
+	}
+}
 
-	// ... COMPLETAR
-	/* Perform any hardware setup necessary. */
-	//prvSetupHardware();
-	/* xTaskCreate(
-					vTask1,  Pointer to the function that implements the task.
-					"Task 1", Text name for the task. This is to facilitate debugging only.
-					1000, Stack depth - small microcontrollers will use much less stack than this.
-					NULL, This example does not use the task parameter.
-					1, This task will run at priority 1.
-					NULL ); This example does not use the task handle.
-	*/
+int main(void){
 	// Alarmas
-	xTaskCreate(vImprimirAlarma, "imprimirAlarma", 200, NULL, tskIDLE_PRIORITY+1, &xImprimirAlarmaHandle);
-	xTaskCreate(vNotificarAlarma, "notificarAlarma", 200, NULL, tskIDLE_PRIORITY+1, NULL);
+	xTaskCreate(vImprimirAlarma, "imprimirAlarma", 200, NULL, tskIDLE_PRIORITY+2, &xImprimirAlarmaHandle);
+	xTaskCreate(vNotificarAlarma, "notificarAlarma", 200, NULL, tskIDLE_PRIORITY+2, NULL);
 	// Info
 	xTaskCreate(vEnviarInfo, "enviarInfo", 200, NULL, tskIDLE_PRIORITY+1, NULL);
 	xTaskCreate(vImprimirInfo, "imprimirInfo", 200, NULL, tskIDLE_PRIORITY+1, NULL);
 	// Respiración
-	xTaskCreate(vRealizarRespiracion, "realizarRespiracion", 200, NULL, tskIDLE_PRIORITY+2, NULL);
-	xTaskCreate(vManejarRespiracion, "manejarRespiracion", 200, NULL, tskIDLE_PRIORITY+2, NULL);
+	xTaskCreate(vRealizarRespiracion, "realizarRespiracion", 200, NULL, tskIDLE_PRIORITY+3, NULL);
+	xTaskCreate(vManejarRespiracion, "manejarRespiracion", 200, NULL, tskIDLE_PRIORITY+3, NULL);
+	xTaskCreate(vAlarmaRespiracion, "alarmaRespiración", 200, NULL, tskIDLE_PRIORITY+2, NULL);
 
 	// Valores iniciales
 	infoQueue = xQueueCreate(160, 80); // buffer_size, item_size
@@ -136,6 +136,7 @@ int main(void){
 	//         queue    , buffer       , cant. max de tick que debe permanecer en blocked hasta que haya espacio en la queue. 0 no espera si el buffer está lleno
 	xQueueSend(infoQueue, texto_inicial, (TickType_t) 0);
 	xSemaforoRespiracion = xSemaphoreCreateBinary();
+	xSemaphoreTake(xSemaforoRespiracion, 0);
 	tiempo_respiracion = 1000;
 
 	// Iniciar el Scheduler
